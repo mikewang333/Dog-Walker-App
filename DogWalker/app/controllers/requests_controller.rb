e9d378085e15a5bc96b259386702ce6a18c58e80
Jae Min Kim <jaeminkim1324@berkeley.edu>
2016-11-16 17:32:00 -0800

    lul added some stuff

--- DogWalker/app/controllers/requests_controller.rb
+++ DogWalker/app/controllers/requests_controller.rb
@@ -3,4 +3,17 @@ class RequestsController < ApplicationController
 		@request = Request.new
 		render 'new'
 	end
+
+	def index
+		@requests = Request.all
+	end
+	def create
+
+	end
+
+	def delete
+		r = Request.find(params[:id])
+		r.destroy
+		redirect_to '/'
+	end
 end
