ae59cc18cab4e61afb727c91a8304812e1f0e8bf
mikewang333 <mikewang.ca@gmail.com>
2016-11-28 22:34:00 -0800

    Implemented dog controller

--- DogWalker/app/controllers/dogs_controller.rb
+++ DogWalker/app/controllers/dogs_controller.rb
@@ -1,2 +1,22 @@
 class DogsController < ApplicationController
+	def new
+        @dog = Dog.new
+    end
+
+    def create
+        @dog = Dog.new(dog_params)
+        @dog.user_id = current_user.id
+
+        if @dog.save
+            redirect_to current_path(current_user.id)
+        else
+            redirect_to new_dog_path
+            flash[:error] = @dog.errors.full_messages.to_sentence
+        end
+    end
+
+    private
+    def dog_params
+        params.require(:dog).permit(:name, :age, :breed)
+    end
 end
