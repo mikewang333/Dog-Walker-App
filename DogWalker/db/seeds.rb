afe2ef49cd7131015a4e48e65c0af8f75bdce654
Lavanya Mittal <lavamitt@berkeley.edu>
2016-11-29 22:33:45 -0800

    Working on seed file

--- DogWalker/db/seeds.rb
+++ DogWalker/db/seeds.rb
@@ -6,4 +6,14 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.new
+User.create([{ name: 'Mike', age: 19, location: 'Berkeley' }, { name: 'Jaemin', age: 19, location: 'Berkeley'}, { name: 'Lavanya', age: 19, location: 'Berkeley'}])
+
+#user1 = User.create( name: 'Mike', age: 19, location: 'Berkeley' )
+#user2 = User.create( name: 'Jaemin', age: 19, location: 'Berkeley' )
+#user3 = User.create( name: 'Lavanya', age: 19, location: 'Berkeley' )
+
+#dog1 = Dog.create( name: 'Doggo', age: 5)
+#dog2 = Dog.create( name: 'Mike', age: 1)
+#dog3 = Dog.create( name: 'Pupper', age: 13)
+
+#Dog.create({user_id: user1.id})
