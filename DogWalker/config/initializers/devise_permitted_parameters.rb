7af7c201e41eb32c5d94f7d8cc6c0ff1ad6b9644
mikewang333 <mikewang.ca@gmail.com>
2016-11-29 23:06:38 -0800

    actually finished project now

--- DogWalker/config/initializers/devise_permitted_parameters.rb
+++ DogWalker/config/initializers/devise_permitted_parameters.rb
@@ -9,6 +9,8 @@ module DevisePermittedParameters
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:age])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:location])
     devise_parameter_sanitizer.permit(:account_update, keys: [:name])
     devise_parameter_sanitizer.permit(:account_update, keys: [:age])
     devise_parameter_sanitizer.permit(:account_update, keys: [:location])
