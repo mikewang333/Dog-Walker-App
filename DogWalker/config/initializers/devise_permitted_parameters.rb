0d96e2bf62f88a0863fad043eb2edb273e0f0948
mikewang333 <mikewang.ca@gmail.com>
2016-11-29 22:53:59 -0800

    Fully functional site

---
+++ DogWalker/config/initializers/devise_permitted_parameters.rb
@@ -0,0 +1,19 @@
+module DevisePermittedParameters
+  extend ActiveSupport::Concern
+
+  included do
+    before_filter :configure_permitted_parameters
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:age])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:location])
+  end
+
+end
+
+DeviseController.send :include, DevisePermittedParameters
